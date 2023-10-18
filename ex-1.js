c0ac8797e9c70618a195ef7f44148f78b29c6a1b
Ploy <Ploypailin_mt@hotmail.com>
2023-10-18 21:56:34 +0700

    feat: finished ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,2 +1,18 @@
 // Exercise #1 Declaring Functions
 // Start coding here
+function add (a,b) {
+    const result = a + b;
+    return result;
+};
+function subtract (a,b) {
+    const result = a - b;
+    return result;
+};
+function mutiply (a,b) {
+    const result = a * b;
+    return result;
+};
+function divide (a,b) {
+    const result = a/b;
+    return result;
+};
