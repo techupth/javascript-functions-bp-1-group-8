0d75d03a93d174719b69a3e9fd1ba1c9597fcc6b
Ploy <Ploypailin_mt@hotmail.com>
2023-10-18 22:38:11 +0700

    feat: finished ex-4

--- ex-4.js
+++ ex-4.js
@@ -1,4 +1,27 @@
 // Exercise #4 Grouping Function with Objects
 
 // Start coding here
-let calculator = {};
+let add = (a,b) => {
+    return a + b;
+};
+let subtract = (a,b) => {
+    return a - b;
+};
+let mutiply = (a,b) => {
+    return a * b;
+};
+let divide = (a,b) => {
+    return a/b;
+};
+
+let calculator = {
+    add: add,
+    subtract: subtract,
+    mutiply: mutiply,
+    divide: divide,
+};
+
+let resultAdd = add (10,20)
+console.log(resultAdd)
+let resultDivide = divide (3000,10)
+console.log(resultDivide)
