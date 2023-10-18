22dc3b498ebb676a009fc488bee66f34ca3b568c
Ploy <Ploypailin_mt@hotmail.com>
2023-10-18 22:34:38 +0700

    feat: finished ex-3

--- ex-3.js
+++ ex-3.js
@@ -1,2 +1,14 @@
 // Exercise #3 Declaring Arrow Function
 // Start coding here
+let add = (a,b) => {
+    return a + b;
+};
+let subtract = (a,b) => {
+    return a - b;
+};
+let mutiply = (a,b) => {
+    return a * b;
+};
+let divide = (a,b) => {
+    return a/b;
+};
