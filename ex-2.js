716a5b9490c19dc782a365303cd404696ae0876a
Ploy <Ploypailin_mt@hotmail.com>
2023-10-18 22:20:41 +0700

    feat: finished ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,2 +1,14 @@
 // Exercise #2 Declaring Function Expression
 // Start coding here
+ let add = function (a,b) {
+    return a + b;
+};
+let subtract = function (a,b) {
+    return a - b;
+};
+let mutiply = function (a,b) {
+    return a * b;
+};
+let divide = function (a,b) {
+    return a/b;
+};
