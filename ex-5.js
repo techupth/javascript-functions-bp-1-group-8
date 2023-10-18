7f1a665d65fdca89ee22f9d0c72673b2a1db4326
Ploy <Ploypailin_mt@hotmail.com>
2023-10-18 22:45:42 +0700

    feat: finished ex-5

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,8 @@
 // Exercise #5 Min Function
 // Start coding here
 const minNumber = findMinNumber(10, 20);
+function findMinNumber (a,b) {
+    return Math.min (a,b)
+}
 
 console.log(minNumber); // 10
